b856fac0ca46d82bcb2d4e9d3f83a5d98f6e2071
FelipeJhordan <felipejordan.alves@gmail.com>
2024-06-14 09:59:09 -0300

    project: inicializate project by spring init

---
+++ desafio-backend-java-luizalabs/src/main/java/com/example/desafio_backend_java_luizalabs/DesafioBackendJavaLuizalabsApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafio_backend_java_luizalabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackendJavaLuizalabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackendJavaLuizalabsApplication.class, args);
+	}
+
+}
---
+++ desafio-backend-java-luizalabs/src/test/java/com/example/desafio_backend_java_luizalabs/DesafioBackendJavaLuizalabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafio_backend_java_luizalabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackendJavaLuizalabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
