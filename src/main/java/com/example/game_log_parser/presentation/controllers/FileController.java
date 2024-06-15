cee4637f86f00cfa502ca91ac96953f8457cb22a
FelipeJhordan <felipejordan.alves@gmail.com>
2024-06-14 21:42:41 -0300

    feat: add file anottation in file controller

--- src/main/java/com/example/game_log_parser/presentation/controllers/FileController.java
+++ src/main/java/com/example/game_log_parser/presentation/controllers/FileController.java
@@ -1,5 +1,8 @@
 package com.example.game_log_parser.presentation.controllers;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,14 +10,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.example.game_log_parser.core.domain.usecases.ProcessLinesAndSaveUsecase;
 import com.example.game_log_parser.presentation.dtos.UploadSuccessMessageDto;
+
+import lombok.AllArgsConstructor;
 
 @RequestMapping("api/v1/file")
 @RestController()
+@AllArgsConstructor()
 public class FileController {
 
+    @Autowired()
+    private final ProcessLinesAndSaveUsecase processLinesAndSaveUsecase;
+
     @PostMapping
-    public ResponseEntity<UploadSuccessMessageDto> uploadFile(@RequestParam("file") MultipartFile file) {
-        return ResponseEntity.ok(new UploadSuccessMessageDto("File uploaded successfully", 0));
+    public ResponseEntity<UploadSuccessMessageDto> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+
+        int games_processed = processLinesAndSaveUsecase.execute(file.getInputStream());
+        return ResponseEntity.ok(new UploadSuccessMessageDto("File uploaded successfully", games_processed));
     }
 }
