846fa6f3c1e9b47cbe5534b506e7e0a2654010d5
FelipeJhordan <felipejordan.alves@gmail.com>
2024-06-14 22:25:12 -0300

    feat: create Game database entity

---
+++ src/main/java/com/example/game_log_parser/infra/database/models/GameModel.java
@@ -0,0 +1,28 @@
+package com.example.game_log_parser.infra.database.models;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@Document(collection = "games")
+public class GameModel {
+    @Transient
+    public static final String SEQUENCE_NAME = "game_sequence";
+
+    @Id()
+    private long id;
+
+    private int total_kills;
+
+    private List<String> players;
+
+    private Map<String, Integer> kills;
+}
