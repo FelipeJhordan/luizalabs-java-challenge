4ac103305ce61f7dc683115cf21edd66445419b4
FelipeJhordan <felipejordan.alves@gmail.com>
2024-06-16 09:36:33 -0300

    feat: enable mongo repositories

--- src/main/java/com/example/game_log_parser/application/listeners/GameGenerateIdMongoEventListener.java
+++ src/main/java/com/example/game_log_parser/application/listeners/GameGenerateIdMongoEventListener.java
@@ -4,13 +4,13 @@ package com.example.game_log_parser.application.listeners;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener;
 import org.springframework.data.mongodb.core.mapping.event.BeforeConvertEvent;
+import org.springframework.stereotype.Component;
 
 import com.example.game_log_parser.infra.database.models.GameModel;
 import com.example.game_log_parser.infra.database.repositories.DatabaseSequenceRepository;
 
-import lombok.AllArgsConstructor;
 
-@AllArgsConstructor
+@Component
 public class GameGenerateIdMongoEventListener extends AbstractMongoEventListener<GameModel> {
 
 
--- src/main/java/com/example/game_log_parser/infra/database/repositories/DatabaseSequenceRepository.java
+++ src/main/java/com/example/game_log_parser/infra/database/repositories/DatabaseSequenceRepository.java
@@ -2,14 +2,19 @@ package com.example.game_log_parser.infra.database.repositories;
 
 import java.util.Objects;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Repository;
+
 import static org.springframework.data.mongodb.core.query.Criteria.where;
 import static org.springframework.data.mongodb.core.FindAndModifyOptions.options;
 import com.example.game_log_parser.infra.database.models.DatabaseSequence;
 
+@Repository
 public class DatabaseSequenceRepository {
+  @Autowired
     private MongoOperations mongoOperations;
 
     public long generateSequence(String seqName) {
