40423d02a8b5113887d0d436059ca8312ac39f47
FelipeJhordan <felipejordan.alves@gmail.com>
2024-06-16 09:50:15 -0300

    feat:  implement controller get games and repository methods

---
+++ src/main/java/com/example/game_log_parser/infra/database/repositories/GameRepositoryImp.java
@@ -0,0 +1,48 @@
+package com.example.game_log_parser.infra.database.repositories;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.example.game_log_parser.core.domain.entities.Game;
+import com.example.game_log_parser.core.domain.interfaces.IGameRepository;
+import com.example.game_log_parser.infra.database.mappers.GameMapper;
+import com.example.game_log_parser.infra.database.models.GameModel;
+import com.example.game_log_parser.infra.database.repositories.interfaces.IGameMongoRepository;
+
+
+
+@Repository
+public class GameRepositoryImp implements IGameRepository {
+    private final IGameMongoRepository mongoRepository;
+    
+    @Autowired
+    public GameRepositoryImp(IGameMongoRepository mongoRepository) {
+        this.mongoRepository = mongoRepository;
+    }
+
+    @Override
+    public Game findById(long id) {
+        Optional<GameModel> gameModel =  mongoRepository.findById(id);
+        if(gameModel.isEmpty()) {
+            return null;
+        }
+
+        return GameMapper.toDomain(gameModel.get());
+    }
+
+    @Override
+    public List<Game> findAll() {
+        List<GameModel> gameModels =  mongoRepository.findAll();
+        return gameModels.stream().map(GameMapper::toDomain).collect(Collectors.toList());
+    }
+
+    @Override
+    public void saveBulk(List<GameModel> games) {
+        mongoRepository.saveAll(games);
+    }
+        
+}
--- src/main/java/com/example/game_log_parser/presentation/controllers/GameController.java
+++ src/main/java/com/example/game_log_parser/presentation/controllers/GameController.java
@@ -1,24 +1,46 @@
 package com.example.game_log_parser.presentation.controllers;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.game_log_parser.core.domain.entities.Game;
+import com.example.game_log_parser.core.domain.usecases.GetGamesByIdUsecase;
+import com.example.game_log_parser.core.domain.usecases.GetGamesUsecase;
 import com.example.game_log_parser.presentation.dtos.GameDto;
+import com.example.game_log_parser.presentation.mapper.GameDtoMapper;
+
+import lombok.AllArgsConstructor;
 
 @RequestMapping("api/v1/game")
 @RestController
+@AllArgsConstructor
 public class GameController {
+    private final GetGamesUsecase getGamesUsecase;
+    private final GetGamesByIdUsecase getGamesByIdUsecase;
+
     @GetMapping()
     public ResponseEntity<List<GameDto>> getGames() {
-        return ResponseEntity.ok(List.of(new GameDto(), new GameDto()));
+        List<GameDto> gamesDtos = getGamesUsecase.execute().stream().map(GameDtoMapper::toDto).collect(Collectors.toList());
+
+        return ResponseEntity.ok(gamesDtos);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<GameDto> getGameById() {
-        return ResponseEntity.ok(new GameDto());
+    public ResponseEntity<GameDto> getGameById(@PathVariable final long id) {
+        Game game = getGamesByIdUsecase.execute(id);
+        
+        if(game == null) {
+            return ResponseEntity.notFound().build();
+        }
+        
+        GameDto gameDto = GameDtoMapper.toDto(game);
+        
+        return ResponseEntity.ok(gameDto);
     }
 }
